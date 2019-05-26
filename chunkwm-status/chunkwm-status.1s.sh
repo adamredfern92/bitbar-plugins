c00c936bbf3337d16e694ff212d304d45a3b5421
Adam Redfern <adamredfern92@gmail.com>
2019-05-26 16:43:20 +0100

    Added new line from title to fix being repeated in dropdown

--- chunkwm-status/chunkwm-status.1s.sh
+++ chunkwm-status/chunkwm-status.1s.sh
@@ -36,7 +36,8 @@ done
 desktop_list="${desktop_list/$(chunkc tiling::query --desktop id)/\e[30;47m$(chunkc tiling::query --desktop id)\e[0:40m\e[1;30m}"
 desktop_list="${desktop_list/F/\e[30;47mFf(chunkc tiling::query --desktop idf(chunkc tiling::query --desktop id))\e[0:40m\e[1;30m}"
 
-printf "\e[1;30m[$(chunkc tiling::query --desktop mode | cut -c 1-3):$desktop_list ] | ansi=true "
+printf "\e[1;30m[$(chunkc tiling::query --desktop mode | cut -c 1-3):$desktop_list ] | ansi=true \n"
+
 echo "---"
 echo "Restart chunkwm & skhd | bash='$0' param1=restart terminal=false"
 echo "Stop chunkwm & skhd | bash='$0' param1=stop terminal=false"
